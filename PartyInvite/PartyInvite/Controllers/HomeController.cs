4ebbb633450990f165c6d99249a8a0f876122b8f
MorrisB-- <morris.ballenger@yahoo.com>
2016-09-06 19:53:32 -0500

    Undid last commit

--- PartyInvite/PartyInvite/Controllers/HomeController.cs
+++ PartyInvite/PartyInvite/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace PartyInvite.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello World";
+            return View();
         }
     }
 }
